0142c9ce50e3bb6e1c988d0f9b64fff7514f4ad9
TretiakovAnat <tretiakovcroissants@gmail.com>
2024-09-16 08:59:38 +0300

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AppRouters from "./AppRouters";
+
+const App = () => {
+  return (
+    <div className="conteiner">
+      <AppRouters />
+    </div>
+  );
+};
+
+export default App;
